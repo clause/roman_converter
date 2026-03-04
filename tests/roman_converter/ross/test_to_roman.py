9b37b38d884868ffb9a3913303ad68294336d7f1
renross <briross@udel.edu>
2026-03-04 16:42:39 -0500

    Test functions split into one function per test

--- tests/roman_converter/ross/test_to_roman.py
+++ tests/roman_converter/ross/test_to_roman.py
@@ -1,14 +1,30 @@
 from types import ModuleType
 
+# Base Case Coverage
+def test_to_roman_base(impl: ModuleType):
+    # uses all letters at least once
+    assert impl.to_roman(1447) == "MCDXLVII"
 
-def test_to_roman(impl: ModuleType):
-    # Base Case Coverage - Variations
-    assert impl.to_roman(1447) == "MCDXLVII" # uses all letters at least once
-    assert impl.to_roman(2447) == "MMCDXLVII" # Change thousands value
-    assert impl.to_roman(1547) == "MDXLVII" # Change hundreds value
-    assert impl.to_roman(1427) == "MCDXXVII" # Change tens value
-    assert impl.to_roman(1449) == "MCDXLIX" # Change ones value
-    # Base Case Coverage - Boundaries
-    assert impl.to_roman(1) == "I" # Lower boundary
-    assert impl.to_roman(3999) == "MMMCMXCIX" # Upper boundary
+def test_to_roman_thousands(impl: ModuleType):
+    # Change thousands value
+    assert impl.to_roman(2447) == "MMCDXLVII"
 
+def test_to_roman_hundreds(impl: ModuleType):
+    # Change hundreds value
+    assert impl.to_roman(1547) == "MDXLVII"
+
+def test_to_roman_tens(impl: ModuleType):
+    # Change tens value
+    assert impl.to_roman(1427) == "MCDXXVII"
+
+def test_to_roman_ones(impl: ModuleType):
+    # Change ones value
+    assert impl.to_roman(1449) == "MCDXLIX"
+
+def test_to_roman_lbound(impl: ModuleType):
+    # Lower boundary
+    assert impl.to_roman(1) == "I"
+
+def test_to_roman_ubound(impl: ModuleType):
+    # Upper boundary
+    assert impl.to_roman(3999) == "MMMCMXCIX"
